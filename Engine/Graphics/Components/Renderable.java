443a4e012bf8aa282db5f6223aa0f865be405f33
Diego Revilla <diego.revilla.r@gmail.com>
2023-09-19 11:17:15 +0200

    Renderable Interface

--- Engine/Graphics/Components/Renderable.java
+++ Engine/Graphics/Components/Renderable.java
@@ -1,3 +1,11 @@
+//
+//	Renderable.java
+//	Legend Of Zelda
+//
+//	Created by Diego Revilla on 19/09/2023
+//	Copyright Deusto © 2023. All Rights reserved
+//
+
 package Engine.Graphics.Components;
 
 import java.awt.Graphics2D;
