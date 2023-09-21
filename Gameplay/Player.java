76571faf69828c24c9cf8f2c6fc3e68419a683ba
Lingfeng555 <121032780+Lingfeng555@users.noreply.github.com>
2023-09-21 16:24:26 +0200

    modified:   Gameplay/Player.java

--- Gameplay/Player.java
+++ Gameplay/Player.java
@@ -14,23 +14,51 @@ import Engine.Input.InputManager;
 import Engine.Math.Vector2D;
 
 public class Player extends Actor {
+    
+    /*  Animation types
+        The difference betwwen the direction and attack is 5
+     */
     private final int RIGHT = 0;
     private final int LEFT = 1;
     private final int DOWN = 2;
     private final int UP = 3;
-    protected boolean up = false;
-    protected boolean down = false;
-    protected boolean right = false;
-    protected boolean left = false;
-    protected int mCurrentAnimation;
-    protected AnimationMachine mAnimation;
+    private final int FALL = 4;
+    private final int ATTACK_RIGHT  = 5;
+    private final int ATTACK_LEFT  = 6;
+    private final int ATTACK_DOWN  = 7;
+    private final int ATTACK_UP  = 8;
+    //----------------------------------------------------------------------
 
-    private boolean arc;
-    private boolean lighter;
-    private boolean bomb;
+    /*  Movement Boolean
+        Used to confirm the direction
+     */
+    private boolean up = false;
+    private boolean down = false;
+    private boolean right = false;
+    private boolean left = false;
+    //----------------------------------------------------------------------
+
+    /* Animation
+     */
+    private int mCurrentAnimation;
+    protected AnimationMachine mAnimation;
+    final private int delay = 10; //This is the delay of the all animations
+    //----------------------------------------------------------------------
+
+    /*  Enable skills
+     */
+    private boolean haveArc;
+    private boolean haveLighter;
+    private boolean HaveBomb;
+    //----------------------------------------------------------------------
     
+    /* Player Stats
+     * 
+     */
     protected AtomicInteger healthPoints = new AtomicInteger(10);
     final private  int damage = 2;
+    private int velocity = 0;
+    //----------------------------------------------------------------------
 
 
     /*! Conversion Constructor
@@ -46,7 +74,7 @@ public class Player extends Actor {
         //---------------------------------------------------------------------
 
         mAnimation = AddComponent(new AnimationMachine(this, sprite));
-        SetAnimation(LEFT, sprite.GetSpriteArray(RIGHT), 50);
+        SetAnimation(LEFT, sprite.GetSpriteArray(LEFT), delay);
 
         InputManager.SubscribePressed(KeyEvent.VK_W, new InputFunction() {
             @Override
@@ -102,24 +130,33 @@ public class Player extends Actor {
     public void Animate() {
         if(up) {
             if(mCurrentAnimation != UP || mAnimation.GetAnimation().GetDelay() == -1) {
-                SetAnimation(UP, mAnimation.GetSpriteSheet().GetSpriteArray(UP), 5);
+                SetAnimation(UP, mAnimation.GetSpriteSheet().GetSpriteArray(UP), delay);
             }
         } else if(down) {
             if(mCurrentAnimation != DOWN || mAnimation.GetAnimation().GetDelay() == -1) {
-                SetAnimation(DOWN, mAnimation.GetSpriteSheet().GetSpriteArray(DOWN), 5);
+                SetAnimation(DOWN, mAnimation.GetSpriteSheet().GetSpriteArray(DOWN), delay);
             }
         } else if(right) {
             if(mCurrentAnimation != RIGHT || mAnimation.GetAnimation().GetDelay() == -1) {
-                SetAnimation(RIGHT, mAnimation.GetSpriteSheet().GetSpriteArray(RIGHT), 5);
+                SetAnimation(RIGHT, mAnimation.GetSpriteSheet().GetSpriteArray(RIGHT), delay);
             }
         } else {
             if(mCurrentAnimation != LEFT || mAnimation.GetAnimation().GetDelay() == -1) {
-                SetAnimation(LEFT, mAnimation.GetSpriteSheet().GetSpriteArray(LEFT), 5);
+                SetAnimation(LEFT, mAnimation.GetSpriteSheet().GetSpriteArray(LEFT), delay);
             }
         }
+        //Attack animation set
+        /* 
+        InputManager.SubscribePressed(KeyEvent.VK_K, new InputFunction() {
+            @Override
+            public void Execute() {
+                SetAnimation(LEFT+5, mAnimation.GetSpriteSheet().GetSpriteArray(LEFT+5), delay);
+            }
+        });
+        */
     }
-
     // ------------------------------------------------------------------------
+
     /*! Update
     *
     *   Adds Behavior to the Player
@@ -129,39 +166,40 @@ public class Player extends Actor {
         super.Update();
         Move();
         Animate();
-        mAnimation.GetAnimation().SetDelay(50);
+        mAnimation.GetAnimation().SetDelay(delay);
     }
-
     // ------------------------------------------------------------------------
+
     /*! Move
     *
     *   Moves the sprite on a certain direction
     */ //----------------------------------------------------------------------
     public void Move() {
         Vector2D<Float> pos = GetPosition();
-        System.out.println(pos.toString());
+        //System.out.println(pos.toString());
         if(up) {
-            pos.y -= 1;
+            pos.y -= velocity;
         }
 
         if(down) {
-            pos.y += 1;
+            pos.y += velocity;
         }
 
         if(left) {
-            pos.x -= 1;
+            pos.x -= velocity;
         }
 
         if(right) {
-            pos.x += 1;
+            pos.x += velocity;
         }
 
         SetPosition(pos);
     }
     // ------------------------------------------------------------------------
+
     /*! Transpose
-    *
-    *   ret -> transposed BufferedImage 2D Matrix
+    *       @Param  -> BufferedImage 2D Matrix
+    *       ret     -> Transposed BufferedImage 2D Matrix
     */ //----------------------------------------------------------------------
     private BufferedImage[][] transposeMatrix(BufferedImage [][] m){
         BufferedImage[][] temp = new BufferedImage[m[0].length][m.length];
@@ -170,4 +208,61 @@ public class Player extends Actor {
                 temp[j][i] = m[i][j];
         return temp;
     }
+    // ------------------------------------------------------------------------
+
+    /* Info. functions
+     * 
+     */
+    @Override
+    public String toString() {
+        return "Player [up=" + up + ", down=" + down + ", right=" + right + ", left=" + left + ", mCurrentAnimation="
+                + mCurrentAnimation + "]";
+    }
+    //------------------------------------------------------------------------
+
+    /* Getters
+     * 
+     */
+    public boolean isUp() {
+        return up;
+    }
+    public boolean isDown() {
+        return down;
+    }
+    public boolean isRight() {
+        return right;
+    }
+    public boolean isLeft() {
+        return left;
+    }
+    public int getmCurrentAnimation() {
+        return mCurrentAnimation;
+    }
+    public int getDelay() {
+        return delay;
+    }
+    public boolean isHaveArc() {
+        return haveArc;
+    }
+    public boolean isHaveLighter() {
+        return haveLighter;
+    }
+    public boolean isHaveBomb() {
+        return HaveBomb;
+    }
+    public int getVelocity() {
+        return velocity;
+    }
+    //------------------------------------------------------------------------
+
+    /* Setters
+     * 
+     */
+    public void setHealthPoints(AtomicInteger healthPoints) {
+        this.healthPoints = healthPoints;
+    }
+    public void setVelocity(int velocity) {
+        this.velocity = velocity;
+    }
+    //------------------------------------------------------------------------
 }
