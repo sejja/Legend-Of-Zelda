c03402a142a79fb6135919ae6018c7345c27b46b
Diego Revilla <diego.revilla.r@gmail.com>
2023-09-25 10:35:41 +0200

    A proper test room

--- Engine/StateMachine/States/PlayState.java
+++ Engine/StateMachine/States/PlayState.java
@@ -37,7 +37,7 @@ public class PlayState extends State {
     *   Just assigns the statemachine child
     */ //----------------------------------------------------------------------
     public PlayState() {
-        mTilemap = new TileManager("Content/TiledProject/StressTest.tmx");
+        mTilemap = new TileManager("Content/TiledProject/TestRoom.tmx");
         mFont =(FontObject)ObjectManager.GetObjectManager().AddEntity(new FontObject("Content/Fonts/ZeldaFont.png", "THE LEGEND OF ANDONI"));
         mFont.SetPosition(new Vector2D<>(100.f, 100.f));
         mFont.SetScale(new Vector2D<>(32.f, 32.f));
