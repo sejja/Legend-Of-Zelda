aa0fadbdd2d002a1cbbdb8db7a4c9da2c478ae84
Lingfeng555 <121032780+Lingfeng555@users.noreply.github.com>
2023-10-10 13:11:23 +0200

    modified:   Content/Animations/bomb.png 	modified:   Engine/Graphics/Spritesheet.java 	new file:   Gameplay/AnimatedObject/Bomb.java

---
+++ Gameplay/AnimatedObject/Bomb.java
@@ -0,0 +1,27 @@
+package Gameplay.AnimatedObject;
+
+import java.awt.image.BufferedImage;
+import java.nio.Buffer;
+
+import Engine.Graphics.Spritesheet;
+import Engine.Math.Vector2D;
+
+public class Bomb extends AnimatedObject {
+    private BufferedImage[][] allAnimtion;
+    public Bomb(Vector2D<Float> position) {
+        super(new Vector2D<>(position));
+        Spritesheet spritesheet = new Spritesheet("Content/Animations/bomb.png", 10,1, true);
+        setAnimationMachine(spritesheet);
+        this.allAnimtion  = spritesheet.GetSpriteArray2D();
+        createChargeAnimation();
+    }
+    private void createChargeAnimation(){
+        for ( int i = 0; i < allAnimtion[0].length; i++){
+            if (i%2 == 0){
+                allAnimtion[1][i] = allAnimtion[0][0];
+            }else{
+                allAnimtion[1][i] = allAnimtion[0][1];
+            }
+        }
+    }
+}
