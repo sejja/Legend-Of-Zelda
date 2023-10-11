a464588daf102c2708703caa9e50738574fa828c
Diego Revilla <diego.revilla.r@gmail.com>
2023-10-11 10:05:46 +0200

    Animation Event Documented

--- Engine/Graphics/Animations/AnimationEvent.java
+++ Engine/Graphics/Animations/AnimationEvent.java
@@ -1,3 +1,11 @@
+//
+//	AnimationEvent.java
+//	Legend Of Zelda
+//
+//	Created by Diego Revilla on 11/10/2023
+//	Copyright Deusto © 2023. All Rights reserved
+//
+
 package Engine.Graphics.Animations;
 
 public interface AnimationEvent {
