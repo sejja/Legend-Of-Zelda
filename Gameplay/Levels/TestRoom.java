77479b10deef07516c99ccd91bde96e81f8b79c0
Asier Larrazabal <asier.larrazabal@opendeusto.es>
2023-10-18 23:33:42 +0200

    Room1

--- Gameplay/Levels/TestRoom.java
+++ Gameplay/Levels/TestRoom.java
@@ -32,7 +32,7 @@ public class TestRoom extends Level {
     public TestRoom(Level right, Level left, Level up, Level down, String tiles, Vector2D<Float> pos) {
         super(right, left, up, down, new TileManager(tiles));
         Init(pos);
-        ObjectManager.GetObjectManager().AddEntity(new Player(new Spritesheet("Content/Animations/Link/Link.png"), new Vector2D<Float>(1350f, 800.f), new Vector2D<Float>(100.f, 100.f)));
+        ObjectManager.GetObjectManager().AddEntity(new Player(new Spritesheet("Content/Animations/Link/Link.png"), new Vector2D<Float>(1420f, 800.f), new Vector2D<Float>(100.f, 100.f)));
         ArrayList<String> dialogueArrayList = new ArrayList<String>();
         ArrayList<String> dialogueArrayList2 = new ArrayList<String>();
         
@@ -40,14 +40,14 @@ public class TestRoom extends Level {
         dialogueArrayList.add("Ahora embarcate en una nueva aventura junto a tu espada y tu \narco");
         dialogueArrayList2.add("Muy buenas caballero] mi nombre es Juan] y estoy aqui protegiendo \nla puerta de acceso al palacio");
         dialogueArrayList2.add("A si que abandona este lugar por favor");
-        ObjectManager.GetObjectManager().AddEntity(new Npc("Aelarion", new Spritesheet("Content/Animations/NPC/NPC_old.png", 68, 72), new Vector2D<Float>(1415.f, 725.f), dialogueArrayList, new Vector2D<Float>(78.f, 78.f), DOWN, squareMovement) );
-        ObjectManager.GetObjectManager().AddEntity(new Npc("Juan", new Spritesheet("Content/Animations/NPC/NPC_boy.png", 64, 64), new Vector2D<Float>(1200f, 900.f), dialogueArrayList2, new Vector2D<Float>(78.f, 78.f),DOWN, yLineMovement) );
+        ObjectManager.GetObjectManager().AddEntity(new Npc("Aelarion", new Spritesheet("Content/Animations/NPC/NPC_old.png", 68, 72), new Vector2D<Float>(1330.f, 800.f), dialogueArrayList, new Vector2D<Float>(78.f, 78.f), UP, yLineMovement) );
+        ObjectManager.GetObjectManager().AddEntity(new Npc("Juan", new Spritesheet("Content/Animations/NPC/NPC_boy.png", 64, 64), new Vector2D<Float>(1580.f, 1550.f), dialogueArrayList2, new Vector2D<Float>(78.f, 78.f),4, stop) );
         ObjectManager.GetObjectManager().Update();
         ObjectManager.GetObjectManager().AddEntity(new Rock(new Vector2D<>(500f, 500f)));
-        /*
-        ObjectManager.GetObjectManager().AddEntity(new GreenKnight(new Vector2D<Float>(450.f, 300.f)));
-        ObjectManager.GetObjectManager().AddEntity(new GreenKnight(new Vector2D<Float>(550.f, 300.f)));
-        ObjectManager.GetObjectManager().AddEntity(new GreenKnight(new Vector2D<Float>(650.f, 300.f)));*/
+        
+        ObjectManager.GetObjectManager().AddEntity(new GreenKnight(new Vector2D<Float>(1300.f, 1670.f)));
+        ObjectManager.GetObjectManager().AddEntity(new GreenKnight(new Vector2D<Float>(1400.f, 1670.f)));
+        ObjectManager.GetObjectManager().AddEntity(new GreenKnight(new Vector2D<Float>(1500.f, 1670.f)));
         var z = (ZeldaCameraComponent) GraphicsPipeline.GetGraphicsPipeline().GetCamera();
 
         Vector2D<Float> topright = new Vector2D<>(GetBounds().GetPosition().x + 1280.f / 2, GetBounds().GetPosition().y + 720.f / 2);
