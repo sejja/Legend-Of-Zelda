861080c19f5cbbddc1054db14fdd461b2deb3c33
Lingfeng555 <121032780+Lingfeng555@users.noreply.github.com>
2023-10-24 15:07:16 +0200

    modified:   Engine/Graphics/Tile/ShadowLayer.java

--- Engine/Graphics/Tile/ShadowLayer.java
+++ Engine/Graphics/Tile/ShadowLayer.java
@@ -14,43 +14,23 @@ public class ShadowLayer {
 
     }
     public static void Render(Graphics2D g, CameraComponent mCamera) {
+
         final int scaleX = 64;
         final int scaleY = 64;
 
-        var cameracoord = mCamera.GetCoordinates();
-        var campos = mCamera.GetParent().GetPosition();
-        Vector2D<Float> tilemapPos = Level.mCurrentLevel.GetBounds().GetPosition();
-        
-        int PosX = (int)(float)(campos.x - cameracoord.x);
-        int PosY = (int)(float)(campos.y - cameracoord.y);
-        
-        g.setColor(Color.BLUE);
-        g.fillRect((int)(float)campos.x, (int)(float)campos.y, 100,100);
-        g.setColor(Color.RED);
-        g.drawRect((int)(float)cameracoord.x, (int)(float)cameracoord.y, 10,10);
-
         Color c = new Color(0,0,0,50);
-
-        PosX -= mCamera.GetDimensions().x/2;
-        PosY -= mCamera.GetDimensions().y/2;
-        g.setColor(Color.magenta);
-        g.drawRect(PosX, PosY, 10,10);
-
         int cameraSizeX = mCamera.GetDimensions().x;
         int cameraSizeY = mCamera.GetDimensions().y;
-
-
         g.setColor(c);
-        for (int drawPointX = PosX; drawPointX < cameraSizeX; drawPointX+=scaleX ){
-           for(int drawPointY = PosY; drawPointY < cameraSizeY; drawPointY+= scaleY){
-                g.fillRect(drawPointX, drawPointY, scaleX, scaleY);
+
+
+
+        for (int drawPointX = 0; drawPointX < cameraSizeX; drawPointX+=scaleX ){
+           for(int drawPointY = 0; drawPointY < cameraSizeY; drawPointY+= scaleY){
+                g.drawRect(drawPointX, drawPointY, scaleX, scaleY);
            }
         }
     }
-
-    public void setOpacity(int i){
-        opacity = i;
-    }
-
+    
     
 }
