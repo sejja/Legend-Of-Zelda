fe365a70fef01877025468d12b4c4d7953e952d4
Lingfeng555 <lingfeng.chen@opendeusto.es>
2023-10-24 09:48:29 +0200

    modified:   Engine/Graphics/Tile/ShadowLayer.java

--- Engine/Graphics/Tile/ShadowLayer.java
+++ Engine/Graphics/Tile/ShadowLayer.java
@@ -17,13 +17,12 @@ public class ShadowLayer {
         final int scaleX = 16;
         final int scaleY = 16;
         var cameracoord = mCamera.GetCoordinates();
+        var campos = mCamera.GetParent().GetPosition();
         Vector2D<Float> tilemapPos = Level.mCurrentLevel.GetBounds().GetPosition();
-        int x = (int) ((cameracoord.x - tilemapPos.x) / scaleX)-2*scaleX;
+        
+        int x = (int) ((cameracoord.x - tilemapPos.x) / scaleX);
         int y = (int) ((cameracoord.y - tilemapPos.y) / scaleY);
-        for(int i = x; i < mCamera.GetDimensions().x; i+=scaleX) {
-            for(int j = y; j < mCamera.GetDimensions().y; j+=scaleY) {
-                g.fillRect(i, j, x, y);
-            }
-        }
+
+        g.fillRect((int)(float)(campos.x - cameracoord.x), (int)(float)(campos.y - cameracoord.y), scaleX, scaleY);
     }
 }
