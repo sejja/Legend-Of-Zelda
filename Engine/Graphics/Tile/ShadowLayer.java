b7b66f705ae4cb505e6c6c28e6a3c73783814d41
Lingfeng555 <121032780+Lingfeng555@users.noreply.github.com>
2023-10-24 13:50:28 +0200

    modified:   Engine/Graphics/Tile/ShadowLayer.java

--- Engine/Graphics/Tile/ShadowLayer.java
+++ Engine/Graphics/Tile/ShadowLayer.java
@@ -23,9 +23,6 @@ public class ShadowLayer {
         
         int PosX = (int)(float)(campos.x - cameracoord.x);
         int PosY = (int)(float)(campos.y - cameracoord.y);
-
-        System.out.println(campos);
-        System.out.println(cameracoord);
         
         g.setColor(Color.BLUE);
         g.fillRect((int)(float)campos.x, (int)(float)campos.y, 100,100);
@@ -54,31 +51,6 @@ public class ShadowLayer {
     public void setOpacity(int i){
         opacity = i;
     }
-    /* 
-    public static void Render(Graphics2D g, CameraComponent camerapos) {
-        //if(true){return;}
-        var cameracoord = camerapos.GetCoordinates();
-        int mTileHeight = 64;
-        int mTileWidth = 64;
 
-        Vector2D<Float> tilemappos = Level.mCurrentLevel.GetBounds().GetPosition();
-
-        int x = (int) ((cameracoord.x - tilemappos.x) / mTileWidth);
-        int y = (int) ((cameracoord.y - tilemappos.y) / mTileHeight);
-
-        System.out.println(camerapos.GetDimensions().x);
-        System.out.println(camerapos.GetDimensions().y);
-        
-        for(int i = x; i < x + (camerapos.GetDimensions().x) + 1; i+=mTileWidth) {
-            for(int j = y; j < y + (camerapos.GetDimensions().y) + 2; j+=mTileHeight) {
-                    int blockx = i ;
-                    int blocky = j ;
-
-                    g.setColor(new Color(0, 0, 0, 100));
-                    g.fillRect(blockx, blocky, mTileWidth, mTileHeight);
-                }
-        }
-        
-    }
-    */
+    
 }
