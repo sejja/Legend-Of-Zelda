5cc4159bf1610ce09089fdfa164e9b54d4461947
Asier-Larrazabal <asier.larrazabal@opendeusto.es>
2023-11-06 10:18:39 +0100

    modified:   Gameplay/Levels/OverWorld1Left.java

--- Gameplay/Levels/OverWorld1Left.java
+++ Gameplay/Levels/OverWorld1Left.java
@@ -1,14 +1,14 @@
 package Gameplay.Levels;
 
-import Engine.ECSystem.Level;
 import Engine.ECSystem.ObjectManager;
+import Engine.ECSystem.World;
 import Engine.Graphics.Tile.TileManager;
 import Engine.Math.Vector2D;
 import Gameplay.Enemies.Units.GreenKnight;
 
-public class OverWorld1Left extends Level {
+public class OverWorld1Left extends World {
 
-    public OverWorld1Left(Level right, Level left, Level up, Level down, String tiles) {
+    public OverWorld1Left(World right, World left, World up, World down, String tiles) {
         super(right, left, up, down, new TileManager(tiles));
     }
     
