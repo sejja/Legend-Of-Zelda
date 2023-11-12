3eb46484812ca342bee39adcc8a81480fe1eea6e
iker-dominguez <iker.dominguez@opendeusto.es>
2023-11-12 19:37:01 +0100

    Overworld + Dungeon

--- Gameplay/States/PlayState.java
+++ Gameplay/States/PlayState.java
@@ -53,9 +53,12 @@ public class PlayState extends State {
         var pasilloIzqAbajo = new TestRoom2(null, null, null, null, "Content/TiledProject/pasilloIzqAbajo.tmx");
         var entradaArriba = new TestRoom2(null, null, null, null, "Content/TiledProject/entradaArriba.tmx");
         var finalRoom = new TestRoom2(null, null, null, null, "Content/TiledProject/finalRoom.tmx");
+        var entrada = new TestRoom2(null, null, null, null, "Content/TiledProject/entrada.tmx");
+
+
 
         var t2 = new OverWorld1Left(null, null, null, null, "Content/TiledProject/OverWorld1Left.tmx");
-        mTestLevel = new TestRoom(t, t2, null, null, "Content/TiledProject/OverWorld1.tmx", new Vector2D<>(0.f, 0.f));
+        mTestLevel = new TestRoom(pasilloDer, t2, null, null, "Content/TiledProject/OverWorld1.tmx", new Vector2D<>(0.f, 0.f));
         
         t2.SetRightLevel(mTestLevel);
        
