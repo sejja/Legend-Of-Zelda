4d0622d475f8d89472239c91ed1c0b9dee332048
Diego Revilla <diego.revilla.r@gmail.com>
2023-11-13 18:23:06 +0100

    Optimized Sprite

--- Engine/Graphics/Sprite.java
+++ Engine/Graphics/Sprite.java
@@ -8,39 +8,19 @@
 
 package Engine.Graphics;
 
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.nio.Buffer;
-import java.util.ArrayList;
-import javax.imageio.ImageIO;
-
 import Engine.Assets.Asset;
 import Engine.Assets.AssetManager;
-import Engine.Math.Vector2D;
 
 public class Sprite { 
-    protected Asset mSpriteSheet = null;
-    protected int mWidth;
-    protected int mHeight;
+    protected Asset mSpriteSheet;
 
     // ------------------------------------------------------------------------
     /*! Constructor
     *
     *   Constructs an Sprite from the filename
     */ //----------------------------------------------------------------------
-    public Sprite(String file) {
-        mSpriteSheet = LoadSprite(file);
-        mWidth = ((BufferedImage)mSpriteSheet.Raw()).getWidth();
-        mHeight = ((BufferedImage)mSpriteSheet.Raw()).getHeight();
-    }
-
-    // ------------------------------------------------------------------------
-    /*! Load Sprite
-    *
-    *   Loads an sprite from the fisk and uploads it as a bufferedimage
-    */ //----------------------------------------------------------------------
-    protected Asset LoadSprite(String file) {
-        return AssetManager.Instance().GetResource(file);
+    public Sprite(final String file) {
+        mSpriteSheet = AssetManager.Instance().GetResource(file);
     }
 
     // ------------------------------------------------------------------------
