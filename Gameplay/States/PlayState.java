969e855e6892c92ba1ed8fae44aabf796309417f
Asier-Larrazabal <asier.larrazabal@opendeusto.es>
2023-11-16 10:11:50 +0100

    new file:   Content/TiledProject/overworldleftup.tmx 	modified:   Gameplay/States/PlayState.java

--- Gameplay/States/PlayState.java
+++ Gameplay/States/PlayState.java
@@ -45,9 +45,13 @@ public class PlayState extends State {
     */ //----------------------------------------------------------------------
     public PlayState() {
         var t = new TestRoom2(null, null, null, null, "Content/TiledProject/TestRoom2.tmx");
-        var t2 = new OverWorld1Left(null, null, null, null, "Content/TiledProject/OverWorld1Left.tmx");
+        var t2 = new TestRoom2(null, null, null, null, "Content/TiledProject/OverWorld1Left.tmx");
+        var t3 = new TestRoom2(null, null, null, null, "Content/TiledProject/overworldleftup.tmx");
+
         mTestLevel = new TestRoom(t, t2, null, null, "Content/TiledProject/OverWorld1.tmx", new Vector2D<>(0.f, 0.f));
         
+        t2.SetUpperLevel(t3);
+        t3.SetLowerLevel(t2);
         t2.SetRightLevel(mTestLevel);
         t.SetLeftlevel(mTestLevel);
 
