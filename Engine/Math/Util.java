6195d21986e176bd8f74db6307f32d693ca5d19c
Diego Revilla <diego.revilla.r@gmail.com>
2023-11-21 12:52:01 +0100

    Optimized LInear Interpolation

--- Engine/Math/Util.java
+++ Engine/Math/Util.java
@@ -1,6 +1,19 @@
+//
+//	Util.java
+//	Legend Of Zeldad
+//
+//	Created by Diego Revilla on 21/11/2023
+//	Copyright Deusto © 2023. All Rights reserved
+//
+
 package Engine.Math;
 
 public class Util {
+    // ------------------------------------------------------------------------
+    /*! Linear Interpolate
+    *
+    *   Interpolate between an "a" and "b" values within an alpha value
+    */ //----------------------------------------------------------------------
     static public float LinearInterpolate(float a, float b, float alpha) {
         return a + alpha * (b - a);
     }
