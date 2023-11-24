f873c9df13bce8b722b049a8ef0cb98481d4289c
Diego Revilla <diego.revilla.r@gmail.com>
2023-11-24 08:45:07 +0100

    CollisionResult Documented

--- Engine/Physics/CollisionResult.java
+++ Engine/Physics/CollisionResult.java
@@ -1,3 +1,11 @@
+//
+//	CollisionResult.java
+//	Legend Of Zelda
+//
+//	Created by Diego Revilla on 24/11/2023
+//	Copyright Deusto © 2023. All Rights reserved
+//
+
 package Engine.Physics;
 
 public enum CollisionResult {
