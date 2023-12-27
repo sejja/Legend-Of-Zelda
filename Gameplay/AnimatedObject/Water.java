298c5a2406d67b6625702c41a3d3ec774b684564
iker-dominguez <iker.dominguez@opendeusto.es>
2023-12-27 18:11:07 +0100

    AWA

--- Gameplay/AnimatedObject/Water.java
+++ Gameplay/AnimatedObject/Water.java
@@ -1,33 +1,19 @@
 package Gameplay.AnimatedObject;
 
-import java.awt.image.BufferedImage;
-
 import Engine.Assets.AssetManager;
-import Engine.Audio.Audio;
-import Engine.Audio.Sound;
-import Engine.ECSystem.ObjectManager;
-import Engine.ECSystem.World;
 import Engine.Graphics.Spritesheet;
-import Engine.Graphics.Animations.AnimationEvent;
-import Engine.Graphics.Components.AnimationMachine;
-import Engine.Graphics.Tile.ShadowLayer;
 import Engine.Math.Vector2D;
-import Engine.Physics.ColliderManager;
 import Engine.Physics.StaticPlayerCollision;
 import Engine.Physics.Components.BoxCollider;
-import Gameplay.Interaction;
-import Gameplay.NPC.Npc;
-
 public class Water extends AnimatedObject implements StaticPlayerCollision {
     private BoxCollider hitbox;
-    private int previusFrameCount = 0;
     private Vector2D<Float> pos;
 
     public Water(Vector2D<Float> position) {
-        super(position, new Spritesheet(AssetManager.Instance().GetResource("Content/Animations/agua.png"), 3,2));
+        super(position, new Spritesheet(AssetManager.Instance().GetResource("Content/Animations/agua2.png"), 2,2));
         this.pos = position;
-        delay = 30;
-        this.SetScale(new Vector2D<>(64f,32f));
+        delay = 70;
+        this.SetScale(new Vector2D<>(64f,64f));
         Animate(1);
         this.setDefaultPseudoPosition();
         setPseudoPositionVisible();
@@ -39,7 +25,6 @@ public class Water extends AnimatedObject implements StaticPlayerCollision {
         super.Update();
         hitbox.Update();
         playerCollision(hitbox);
-        
         SetPosition(pos);
     }
 
